2e6bd7116d455b7401df10c5360faf8b882668cc
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2022-01-19 18:50:42 -0800

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,1 +1,18 @@
+;---------------------------------------------|
+; MOS(Minimalistic Operating System           |
+; (c) 2022 GNU GENERAL PUBLIC LICENSE         |
+; DiamondCoder1000                            |
+;                                             |
+;                                             |    
+;---------------------------------------------|
+
+;The includes
+;I know that they are usually placed at the end but i dont care. I'm doing it here :).
+@include src/lib/mem.asm
+@include src/lib/fs.asm
+@include src/lib/io.asm
+@include src/cli.asm
+
+;i sat here procrastinating for like 3 mins
+
 
